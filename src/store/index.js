7ab8a376562d90b89de8148d16afb4c9838c5687
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-11-15 10:36:52 -0300

    Added store actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from 'vuex'
+import router from '../router'
+import {auth} from '../firebase/index'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 
 export default createStore({
   state: {
@@ -13,15 +16,67 @@ export default createStore({
     }
   },
   actions: {
-    // async login ({commit}, details){
-    //   //
-    // },
-    // async logout ({commit}){
-    //   //
-    // },
-    // async register ({commit}, details){
-    //   //
-    // },
+    async login ({commit}, details){
+      const {email, password} = details
+
+      try{
+        await signInWithEmailAndPassword(auth, email, password)
+      } catch (error){
+        switch(error.code){
+          case 'auth/user-not-found':
+            alert("Usuário não encontrado!")
+            break
+          case 'auth/wrong-password':
+            alert("Senha errada! Favor digite novamente.")
+            break
+          default:
+            alert("Algo deu errado.")
+        }
+
+        return
+      }
+
+      commit('SET_USER', auth.currentUser)
+
+      router.push('/')
+    },
+    async logout ({commit}){
+      await signOut(auth)
+
+      commit('CLEAR_USER')
+
+      router.push('/login')
+    },
+    async register ({commit}, details){
+      const {email, password} = details
+
+      try{
+        await createUserWithEmailAndPassword(auth, email, password)
+      } catch (error){
+        switch(error.code){
+          case 'auth/email-already-in-use':
+            alert("Email já registrado, favor use outro ou faça o login!")
+            break
+          case 'auth/invalid-email':
+            alert("Email inválido.")
+            break
+          case 'auth/operation-not-allowed':
+            alert("Operação não permitida.")
+            break
+          case 'auth/weak-password':
+            alert("Senha muito fraca.")
+            break
+          default:
+            alert("Algo deu errado.")
+        }
+
+        return
+      }
+
+      commit('SET_USER', auth.currentUser)
+
+      router.push('/')
+    },
 
   }
 })
